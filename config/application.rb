da71e25014ad40d6542827bcec8b2edb942c37fb
developer1062 <ankithamaragharr@gmail.com>
2025-09-02 18:00:31 +0530

    changed app/views/pwa/manifest.json.erb

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'open-uri'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module HamaraGroup
+module Lands Lords
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
